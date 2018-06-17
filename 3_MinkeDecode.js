281d8b9e21ac350a393c81eb51f6d05bfc8b3b2c
margaritov <lubo@hush.com>
2018-06-17 20:26:47 +0300

    added comments to solution 3

--- 3_MinkeDecode.js
+++ 3_MinkeDecode.js
@@ -1,22 +1,22 @@
 ﻿function minkeDecode(input) {
-    let startIndex = Number(input[0]);
-    let endIndex = Number(input[1]);
-    let missingWord = input[2];
-    let encryptedText = input[3]
-    let patternWord = /\b[A-Z][A-za-z]+[A-Z]\b/g;
-    let encryptedWord = patternWord.exec(encryptedText).toString();
-    let frontPart = encryptedWord.substr(0, startIndex);
-    let secondPart = encryptedWord.substring(endIndex + 1);
-    let decoded = frontPart + missingWord.toLowerCase() + secondPart.toLowerCase();
-    let numbers = encryptedText.match(/[0-9]{3}\.?[0-9]*/g);
-    let res = '';
-    for (let i = 0; i < numbers.length; i++) {
-        numbers[i] = Math.ceil(Number(numbers[i]));
-        let chr = String.fromCharCode(numbers[i]);
-        if (i === 0) chr = chr.toUpperCase();
-        res += chr;
+    let startIndex = Number(input[0]); // start index of substring to be replaced in first encrypted word
+    let endIndex = Number(input[1]);  // end index of substring to be replaced in first encrypted word
+    let missingWord = input[2]; // substring in first encrypted word to be replaced with this string
+    let encryptedText = input[3]; //encrypted text
+    let patternWord = /\b[A-Z][A-za-z]+[A-Z]\b/g; // pattern to extract first encrypted word from text
+    let encryptedWord = patternWord.exec(encryptedText).toString(); // apply regex pattern to extract first encrypted word
+    let frontPart = encryptedWord.substr(0, startIndex); // front part of first encrypted word
+    let secondPart = encryptedWord.substring(endIndex + 1); // last part of first encrypted word
+    let decoded = frontPart + missingWord.toLowerCase() + secondPart.toLowerCase(); // compose decoded first word
+    let numbers = encryptedText.match(/[0-9]{3}\.?[0-9]*/g); // pattern to extract all three-digit numbers (including float) from text
+    let res = '';  // res will hold second word decrypted
+    for (let i = 0; i < numbers.length; i++) { // iterate over three-digit numbers
+        numbers[i] = Math.ceil(Number(numbers[i])); // round to floor floating point number
+        let chr = String.fromCharCode(numbers[i]); // get ascii char for current number
+        if (i === 0) chr = chr.toUpperCase(); // first char will be uppercase
+        res += chr; // add char to second word decrypted
     }
-    console.log(decoded + ' => ' + res);
+    console.log(decoded + ' => ' + res); // print first and second decrypted word
 }
 
 minkeDecode(["3", "5", "gar",        "114 sDfia 1, riDl10 confin$4%#ed117 likewise it humanity aTe114.223432 BultoriA - Varnd railLery101 an unpacked as he"]);
