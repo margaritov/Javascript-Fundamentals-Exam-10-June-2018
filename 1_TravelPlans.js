333b607111ee13aaef72563cb467fb88a00edae8
margaritov <lubo@hush.com>
2018-06-17 19:59:53 +0300

    added comments in solution 1 - Travel Plans

--- 1_TravelPlans.js
+++ 1_TravelPlans.js
@@ -1,16 +1,17 @@
 function travelPlans(input) {
-    function isSpecialized(profession) {
+
+    function isSpecialized(profession) { // return true if profession is "specialised"
         if (profession === "Programming" || profession === "Hardware maintenance"
             || profession === "Cooking" || profession === "Translating" || profession === "Designing") return true;
         else return false;
     }
 
-    function isAverage(profession) {
+    function isAverage(profession) { // return true if profession is "average"
         if (profession === "Driving" || profession === "Managing" || profession === "Fishing" || profession === "Gardening") return true;
         else return false;
     }
 
-    function isClumsy(profession) {
+    function isClumsy(profession) { // return true if profession is "clumsy"
         if (profession === "Singing" || profession === "Accounting"
             || profession === "Teaching" || profession === "Exam-Making" || profession === "Acting"
             || profession === "Writing" || profession === "Lecturing" || profession === "Modeling" || profession === "Nursing"
@@ -18,32 +19,32 @@ function travelPlans(input) {
         else return false;
     }
 
-    let clumsyCounter = 0;
-    let specializedCounter = 0;
+    let clumsyCounter = 0; // clumsy professions customers counter
+    let specializedCounter = 0; // special professions customers counter
     let totalGold = 0;
-    for (let line of input) {
 
-        let [profession, amount] = line.split(" : ");
-        amount = Number(amount);
-        if (isClumsy(profession)) {
-            clumsyCounter++;
+    for (let line of input) { // process each customer
+        let [profession, amount] = line.split(" : "); // split input string to profession and amount
+        amount = Number(amount); // parse amount no number
+        if (isClumsy(profession)) { // is profession "clumsy"?
+            clumsyCounter++; //increase "clumsy" profession customers counter
             if (clumsyCounter % 2 === 0) {
-                amount *= 0.95;
+                amount *= 0.95; // every second customer pays 5% less
             }
             else if (clumsyCounter % 3 === 0) {
-                amount *= 0.9;
+                amount *= 0.9; // every third customer pays 10% less
             }
         }
 
-        if (isSpecialized(profession)) {
-            if (amount > 200) {
-                specializedCounter++;
-                amount *= 0.8;
-                if (specializedCounter % 2 === 0) {
-                    amount += 200;
+        if (isSpecialized(profession)) { // specialized?
+            if (amount > 200) { // only work for minimum 200 gold in specialized professions
+                specializedCounter++; //increment specialized counter
+                amount *= 0.8; //decrease income with 20% spent for candies
+                if (specializedCounter % 2 === 0) { // is it second customer?
+                    amount += 200;  // 200 gold tip from every second customer of specialized professions
                 }
             } else {
-                amount = 0;
+                amount = 0; // less than 200 gold offered - no work
             }
         }
         totalGold += amount
